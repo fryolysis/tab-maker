7998e3ed9563df55fe6263709b6637f10c6c49bd
fryolysis <ucanihl@gmail.com>
2023-03-14 17:14:51 +0300

    readme changes + some refactoring in code

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-# tab-maker
+# Tab-maker
 
 This program is intended to produce guitar tabs from midi files such that the total number of horizontal moves of the fretting hand is kept minimal.
+
+# Background
+
+We enumerate hand positions from neck to body starting from 1 to 12. By hand position we mean the position of the index finger. 
+
+1. We assume that there is a fixed number of frets the hand can reach horizontally at any horizontal position, the default value is 4.
+2. We assume that the exact same note (the unison) is never played on different strings.
+3. We assume that the hand position cannot be changed during the playing of a fretted note (not an open string).
+4. We assume that the midi file consists of a single track.
+
+Under these assumptions our code outputs a tablature which shows when to change the hand position to where. The solution is optimum in the sense that it minimizes the number of hand position changes.
+
+# Algorithm
+In the following, midi events refer to 'note_on' or 'note_off' events. Let e_i be the i^{th} midi event in the input. Let S_i be the notes that are playing at the after the i^{th} event. We define a bag to be the union S_i + S_{i+1} + ... + S_j such that S_i and S_j are both playable with open strings only (or possibly empty).
--- tab.py
+++ tab.py
@@ -8,13 +8,21 @@ HAND_COVERAGE = 4
 FRETS_PER_STRING = 12
 open_strings = [i+40 for i in [0,5,10,15,19,24]] # standard tuning
 
+tune = []
+
+@dataclass(order=True)
+class BagItem:
+    rank: int
+    hand_pos: int
+    tune_pos: int
+    parent: None
+    delete: bool
 
 @dataclass()
 class NoteEvent:
     note: int
     is_on: bool
 
-tune = []
 
 # input from midi file
 midi_file = MidiFile('test.mid')
@@ -31,14 +39,7 @@ for msg in track:
         tune.append(NoteEvent(msg.note, False))
 
 
-@dataclass(order=True)
-class BagItem:
-    rank: int
-    hand_pos: int
-    parent: None
-    delete: bool
-    mel_pos: int
-
+''' returns the set of all hand positions that can play the given range of tune '''
 def get_hand_pos(note):
     res = set()
     for h in range(1,FRETS_PER_STRING):
@@ -47,6 +48,40 @@ def get_hand_pos(note):
                 res.add(h)
                 break
     return res
+
+# we make the assumption that hand positions can change only when all notes except open strings are off
+# we further assume that the same note is never played simultaneously on different strings
+cur_notes = set()
+# TODO: don't forget that the initial position is always eligible for hand position change!
+bag = [BagItem(0,h,None,False,0) for h in get_hand_pos(tune[0])]
+heapq.heapify(bag)
+
+for i in range(len(tune)):
+    e = tune[i]
+    if e.is_on:
+        cur_notes.add(e.note)
+    else:
+        cur_notes.remove(e.note)
+    if cur_notes.difference(open_strings):
+        continue
+
+    # remove marked items
+    (bag.remove(i) for i in bag if i.delete)
+
+    new_hand_pos = get_hand_pos(cur_notes)
+    bag_positions = set([i.hand_pos for i in bag])
+    # mark items for deletion
+    for i in bag:
+        if i.hand_pos not in new_hand_pos:
+            i.delete = True
+    # add new items
+    for h in new_hand_pos - bag_positions:
+        item = BagItem(bag[0].rank + 1, h, bag[0], False, mel_pos)
+        heapq.heappush(bag, item)
+
+
+
+
 
 
 bag = [BagItem(0,h,None,False,0) for h in get_hand_pos(melody[0])]
